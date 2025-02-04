dff395c6a6d60b3da76effa1633a6e67cbd7785f
Kaveesha G.D.C <gdchamithkaveesha@gmail.com>
2025-02-04 14:41:26 +0530

    Add box blur and motion blur filters; stub for Gaussian blur filter

---
+++ src/main/java/pixelBlend/filters/MotionBlurFilter.java
@@ -0,0 +1,23 @@
+package pixelBlend.filters;
+
+import java.awt.image.BufferedImage;
+import java.awt.image.ConvolveOp;
+import java.awt.image.Kernel;
+
+public class BoxBlurFilter implements Filter {
+
+
+    @Override
+    public BufferedImage addFilter(BufferedImage image) {
+        float[] BoxBlurKernel = {
+                1/9f, 1/9f, 1/9f,
+                1/9f, 1/9f, 1/9f,
+                1/9f, 1/9f, 1/9f
+        };
+
+
+        Kernel kernel = new Kernel(3, 3, BoxBlurKernel);
+        ConvolveOp op = new ConvolveOp(kernel, ConvolveOp.EDGE_NO_OP, null);
+        return op.filter(image, null);
+    }
+}
