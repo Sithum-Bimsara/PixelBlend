d0bc748d646b67f781c16195683a9165f17dabcd
Kaveesha G.D.C <gdchamithkaveesha@gmail.com>
2025-02-04 15:01:15 +0530

    Implement motion blur filter with custom kernel

--- src/main/java/pixelBlend/filters/MotionBlurFilter.java
+++ src/main/java/pixelBlend/filters/MotionBlurFilter.java
@@ -4,19 +4,20 @@ import java.awt.image.BufferedImage;
 import java.awt.image.ConvolveOp;
 import java.awt.image.Kernel;
 
-public class BoxBlurFilter implements Filter {
-
+public class MotionBlurFilter implements Filter {
 
     @Override
     public BufferedImage addFilter(BufferedImage image) {
-        float[] BoxBlurKernel = {
-                1/9f, 1/9f, 1/9f,
-                1/9f, 1/9f, 1/9f,
-                1/9f, 1/9f, 1/9f
+        float[] MotionBlurKernel = {
+                1/5f, 1/5f, 1/5f, 1/5f, 1/5f,
+                0f, 1/5f, 1/5f, 1/5f, 1/5f,
+                0f, 0f, 1/5f, 1/5f, 1/5f,
+                0f, 0f, 0f, 1/5f, 1/5f,
+                0f, 0f, 0f, 0f, 1/5f
         };
 
 
-        Kernel kernel = new Kernel(3, 3, BoxBlurKernel);
+        Kernel kernel = new Kernel(3, 3, MotionBlurKernel);
         ConvolveOp op = new ConvolveOp(kernel, ConvolveOp.EDGE_NO_OP, null);
         return op.filter(image, null);
     }
