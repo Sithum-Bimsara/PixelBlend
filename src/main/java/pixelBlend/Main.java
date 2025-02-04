178e90b2492a3309e5f7df1f6f9a1e03842bfa39
Ke-vin-S <kevinxsanjula@gmail.com>
2025-02-04 12:16:38 +0530

    Write image

--- src/main/java/pixelBlend/Main.java
+++ src/main/java/pixelBlend/Main.java
@@ -8,11 +8,16 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         try {
-            BufferedImage image = ImageIO.read(new File("pic.png"));
+            String pathname = "pic.png";
+            BufferedImage image = ImageIO.read(new File(pathname));
             System.out.println("Image Loaded Successfully");
             System.out.println("Width: " + image.getWidth());
             System.out.println("Height: " + image.getHeight());
+
+            pathname = "new" + pathname;
+            ImageIO.write(image, "png", new File(pathname));
         } catch (IOException e) {
             System.err.println("Error loading image: " + e.getMessage());
-        }    }
+        }
+    }
 }
